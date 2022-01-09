2e252398573cbd602420fc6471cb5937ae81ec2c
Sriram M <sriram13k@gmail.com>
2022-01-09 23:37:14 +0530

    Use relative addresses

--- tests/test_chess_scanner.py
+++ tests/test_chess_scanner.py
@@ -14,9 +14,9 @@ from glob import glob
 from torchvision import transforms
 import time
 
-NN_MODEL_PATH = "../artifacts/model.pth"
+NN_MODEL_PATH = "artifacts/model.pth"
 FEN_CHARS = "1RNBQKPrnbqkp"
-IMAGE_PATH = "../tests/support/"
+IMAGE_PATH = "tests/support/"
 
 
 def _chessboard_tiles_img_data(chessboard_img_path):
